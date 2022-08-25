7d0c10adb43118d4c57243e5795384e642a0891f
Yordanos Dereje <yordadereje@gmail.com>
2022-08-26 00:10:12 +0300

    130-135

--- binary_trees.h
+++ binary_trees.h
@@ -67,6 +67,11 @@ avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 void sata_helper(avl_t **root, int *array, size_t lo, size_t hi);
+int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
+int btih_helper(const binary_tree_t *tree);
+
 
 
 #endif
