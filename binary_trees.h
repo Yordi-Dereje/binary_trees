8aaad652e3607626fe7481af92d4fd3cff43bcc4
Yordanos Dereje <yordadereje@gmail.com>
2022-08-25 23:51:52 +0300

    121 to 124

--- binary_trees.h
+++ binary_trees.h
@@ -62,7 +62,11 @@ bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
 bst_t *bst_min_val(bst_t *root);
-
+int binary_tree_is_avl(const binary_tree_t *tree);
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+void sata_helper(avl_t **root, int *array, size_t lo, size_t hi);
 
 
 #endif
