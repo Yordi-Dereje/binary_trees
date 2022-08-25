211b3ae4a10171d2e6f5b1dcf8d86e432a419188
Yordanos Dereje <yordadereje@gmail.com>
2022-08-25 21:38:37 +0300

    102

--- binary_trees.h
+++ binary_trees.h
@@ -50,7 +50,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level);
-
+int binary_tree_is_complete(const binary_tree_t *tree);
+int btic_helper(const binary_tree_t *tree, size_t index, size_t size);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 
 #endif
