da05326a008dd5d466e78357fdcdeaaaea59101b
Yordanos Dereje <yordadereje@gmail.com>
2022-08-25 21:31:21 +0300

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,19 +9,19 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
+	if (!parent)
 		return (NULL);
-	if (parent->left == NULL)
-	{
-		parent->left = new;
-	}
-	else
-	{
-		new->left = parent->left;
-		parent->left->parent = new;
-		parent->left = new;
-	}
 
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->right = NULL;
+	new->left = parent->left;
+	parent->left = new;
+	if (new->left)
+		new->left->parent = new;
 	return (new);
 }
